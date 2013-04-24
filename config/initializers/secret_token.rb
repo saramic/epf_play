b91788b549a059937e3f0d5329d9a3bd73213e32
Michael Milewski <michael@bikeexchange.com.au>
2013-04-24 11:42:47 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EpfPlay::Application.config.secret_token = '1c0bcb5513f08e9d192b255cf3ad905cc464dd6e36ce3a4fa27fb370d6b24546a1d992084061571d8847214b0a3923c49f61e51052a8e5424c058acc034d79d8'
