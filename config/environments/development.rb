ff4225161250739ad2f4f09eb8574a89ab1bc282
Michael Milewski <michael@bikeexchange.com.au>
2013-05-03 18:35:54 +1000

    setup email config to work with mailcatcher, probably need to gem install mailcatcher and run it to catch email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,11 @@ EpfPlay::Application.configure do
 
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # use mailcatcher for outgoing email - http://mailcatcher.me/
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
+  #config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
