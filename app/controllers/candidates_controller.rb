91bdfae8cc737f844dfb6c95207d0e3872cfe764
Michael Milewski <michael@bikeexchange.com.au>
2013-05-12 11:21:13 +1000

    added breadcrumbs of sorts

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,9 +1,9 @@
 class CandidatesController < ApplicationController
   def index
     scope = Candidate
-    party = Party.find(params[:party]) if params[:party]
+    @party = Party.find(params[:party]) if params[:party]
     @state = State.find_by_short_name params[:state] if params[:state]
-    scope = scope.where(party_id: party) if party
+    scope = scope.where(party_id: @party) if @party
     scope = scope.where(state_id: @state) if @state
     @listables = scope.all
   end
--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -2,9 +2,9 @@ class PartiesController < ApplicationController
   def index
     scope = Party
     @state = State.find_by_short_name params[:state] if params[:state]
-    party = Party.find params[:party] if params[:party]
+    @party = Party.find params[:party] if params[:party]
     scope = scope.representation_in(@state) if @state
-    scope = scope.where(id: party) if party
+    scope = scope.where(id: @party) if @party
     @listables = scope.all
   end
 
