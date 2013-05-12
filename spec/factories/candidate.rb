799bec878f213f8edf4c131e21181a512e0b7c4e
Michael Milewski <michael@bikeexchange.com.au>
2013-05-12 15:20:40 +1000

    fix factory that would stop seeding a fresh db:reset

--- spec/factories/candidate.rb
+++ spec/factories/candidate.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
     given_name 'John'
     surname 'Citizen'
     association :party, :factory => :party
-    state State.first
+    #state State.find_or_create_by_short_name('VIC')
     sequence(:aec_candidate_id) { |n| n }
   end
 end
