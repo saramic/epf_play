7123cec3e9c46a5fe692a15e41c68779c336c594
Michael Milewski <michael@bikeexchange.com.au>
2013-05-20 00:35:44 +1000

    some ideas for future improvement of state

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,8 @@
 class State < ActiveRecord::Base
   attr_accessible :short_name
 
+  # TODO change short_name to abbreviation ab abrev?
+  # TODO add shortcuts State.act, State.vic etc through method_missing
   def to_s
     short_name
   end
